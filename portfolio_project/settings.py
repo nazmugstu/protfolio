5e9957b9a6b575605451a77e6ce879a1bc4af16f
alam0164088 <168212623+alam0164088@users.noreply.github.com>
2025-05-06 07:23:11 +0600

    Add render domain to ALLOWED_HOSTS

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -9,7 +9,12 @@ SECRET_KEY = 'django-insecure-n!i9d+ieq99@%81=(dh(3mw%42x)n3a_(c2%u20_bkq0pn2h0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myprotfolio-ip7v.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'protfolio-tar6.onrender.com'  # ← এই লাইন যুক্ত করুন
+]
+
 
 
 # Application definition
